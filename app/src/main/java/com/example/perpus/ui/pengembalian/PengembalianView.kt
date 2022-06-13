9f2ba10adac7c1afd0cd62ace029032dbc9d7e8a
Fathur Rochman Zainul <fathurrochman123.fr@gmail.com>
2022-06-13 21:28:16 +0700

    Commit

---
+++ app/src/main/java/com/example/perpus/ui/pengembalian/PengembalianView.kt
@@ -0,0 +1,9 @@
+package com.example.perpus.ui.pengembalian
+
+import com.example.perpus.model.PayloadPeminjaman
+import com.example.perpus.model.PayloadPengembalian
+
+interface PengembalianView {
+    fun onSuccessPengembalian(payloadPengembalian: ArrayList<PayloadPengembalian>?)
+    fun onErrorResponse()
+}
