6c60ea0617e71310f99dabad018e59211eae6d36
DrDrap11 <kmu856@gmail.com>
2022-06-13 20:57:56 +0700

    Commit

--- app/src/main/java/com/example/perpus/MainActivity.kt
+++ app/src/main/java/com/example/perpus/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.belajar.perpus
+package com.example.perpus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.belajar.perpus.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
